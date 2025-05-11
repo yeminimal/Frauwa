03835c66792b0fe198daccf74d4b0e32c56d9aec
Williams Martins <gh0stphas3@proton.me>
2025-05-11 23:53:54 +0000

    Add Analytics integration from @vercel/analytics/next

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -29,6 +29,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
